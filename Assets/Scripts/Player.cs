53cfc70ba1b581938ca26a8074df2ffff1576d29
ArtemisMacDuffie <G00168172@stu.raritanval.edu>
2024-01-30 22:42:42 -0500

    recoil animation, shooting sfx, reticle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,18 +7,28 @@ public class Player : MonoBehaviour
     public float velocity;
     public GameObject projectile;
     public float delay;
+    public AudioClip shootSound;
 
     private Camera cam;
     private float timeSinceLastShot;
+    private Animator animator;
+    private AudioSource sound;
 
     private void Awake()
     {
         cam = GetComponentInChildren<Camera>();
+        animator = GetComponentInChildren<Animator>();
+        sound = GetComponentInChildren<AudioSource>();
     }
 
     private void Update()
     {
         timeSinceLastShot += Time.deltaTime;
+
+        if (animator.GetBool("Shot"))
+        {
+            animator.SetBool("Shot", false);
+        }
 
         if (Input.GetButtonDown("Fire1") && (timeSinceLastShot > delay))
         {
@@ -28,6 +38,8 @@ public class Player : MonoBehaviour
             newBlast.transform.position = cam.transform.position;
             Rigidbody blastRB = newBlast.GetComponent<Rigidbody>();
             blastRB.AddForce(cam.transform.forward * velocity);
+            animator.SetBool("Shot", true);
+            sound.PlayOneShot(shootSound);
         }
     }
 }
